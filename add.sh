2b0d24814fd5407d37fab469f96c4d45ee48079f
guliter <86565392+guliter@users.noreply.github.com>
2021-12-12 19:11:44 +0800

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,10 +1,6 @@
 
 read -p "添加Socks5用户（用户和密码相同）0：" tp
-stty erase '^H' && read -p "请输入宝塔面板添加的网站目录,宝塔默认地址应该是域名（不带http/https）：" website
+a=&tp
+b=/etc/init.d/sockd adduser
+${b} ${a} ${a}
 
-/etc/init.d/sockd adduser &tp &tp
-
-echo -e "tp：http://$tp/Admin/login"
-
-
-echo -e "后台地址：http://$website/Admin/login"
