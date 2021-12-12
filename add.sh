f0c9d6e52b3c4f65a8c7a1e85efa7f67da9d85a7
guliter <86565392+guliter@users.noreply.github.com>
2021-12-12 20:23:11 +0800

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,8 +1,1 @@
-export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-stty erase '^H' && read -p "密码：" mysqlpassword
-a=&'mysqlpassword'
-b='/etc/init.d/sockd adduser'
-
-${b} ${a} ${a}
-
-
+printf "%-30s\n"  "${CGREEN} Socks5 Info:${CEND}"
