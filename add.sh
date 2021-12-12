2c8b9a0a51a7f58b26cb79cb22c9541df7d69660
guliter <86565392+guliter@users.noreply.github.com>
2021-12-12 19:40:46 +0800

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,6 +1,8 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 stty erase '^H' && read -p "密码：" mysqlpassword
-a=&mysqlpassword
-/etc/init.d/sockd adduser ${a} ${a}
+a=&'mysqlpassword'
+b='/etc/init.d/sockd adduser'
+
+${b} ${a} ${a}
 
 
