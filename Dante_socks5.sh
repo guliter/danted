831386c6ab3eb5d434311aa95eb9a27d8a6c67cb
guliter <86565392+guliter@users.noreply.github.com>
2021-12-12 16:09:37 +0800

    Update Dante_socks5.sh

--- Dante_socks5.sh
+++ Dante_socks5.sh
@@ -3,7 +3,28 @@
 #export PATH
 
 
-
+# 设置字体颜色函数
+function blue(){
+    echo -e "\033[34m\033[01m $1 \033[0m"
+}
+function green(){
+    echo -e "\033[32m\033[01m $1 \033[0m"
+}
+function greenbg(){
+    echo -e "\033[43;42m\033[01m $1 \033[0m"
+}
+function red(){
+    echo -e "\033[31m\033[01m $1 \033[0m"
+}
+function redbg(){
+    echo -e "\033[37;41m\033[01m $1 \033[0m"
+}
+function yellow(){
+    echo -e "\033[33m\033[01m $1 \033[0m"
+}
+function white(){
+    echo -e "\033[37m\033[01m $1 \033[0m"
+}
 
 #工具安装
 install_pack() {
